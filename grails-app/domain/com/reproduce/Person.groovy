69e6af9463e3b5f4f13c843505d7e02d6c0aca69
Tobias Kraft <tobias.kraft@exensio.de>
2021-01-26 08:46:53 +0100

    reproduce version bug

---
+++ grails-app/domain/com/reproduce/Person.groovy
@@ -0,0 +1,12 @@
+package com.reproduce
+
+class Person {
+
+    String firstName
+    String lastName
+
+    static constraints = {
+    }
+
+
+}
---
+++ grails-app/init/versionbug/BootStrap.groovy
@@ -0,0 +1,9 @@
+package versionbug
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
